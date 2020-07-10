82ce3115913e67002a1ac022048b9c55f0259ad4
theNcoder <nisalwickramage@gmail.com>
2020-07-10 18:02:08 +0800

    Added domain model and db model for employee.

---
+++ src/Techhunt.SalaryManagement.Domain/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Techhunt.SalaryManagement.Domain
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
---
+++ src/Techhunt.SalaryManagement.Infrastructure/Persistance/EmployeeDbModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Techhunt.SalaryManagement.Domain;
+
+namespace Techhunt.SalaryManagement.Infrastructure.Persistance
+{
+    public class EmployeeDbModel : Employee
+    {
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
