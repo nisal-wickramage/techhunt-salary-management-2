f9d61426d20f0b46d5365dac2bf05b0b97b1f0ef
theNcoder <nisalwickramage@gmail.com>
2020-07-10 22:15:57 +0800

    Created db configuration for employee db model.

---
+++ src/Techhunt.SalaryManagement.Infrastructure/Persistance/EmployeeDbConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Techhunt.SalaryManagement.Infrastructure.Persistance
+{
+    public class EmployeeDbConfiguration : IEntityTypeConfiguration<EmployeeDbModel>
+    {
+        public void Configure(EntityTypeBuilder<EmployeeDbModel> builder)
+        {
+            builder.ToTable("Employees","SalaryManagement");
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Id)
+                .HasColumnName("Id")
+                .HasColumnType("nvarchar(6)")
+                .IsRequired();
+
+            builder.Property(e => e.Login)
+                .HasColumnName("Login")
+                .HasColumnType("nvarchar(50)")
+                .IsRequired();
+
+            builder.Property(e => e.Name)
+                .HasColumnName("Name")
+                .HasColumnType("nvarchar(200)")
+                .IsRequired();
+
+            builder.Property(e => e.Salary)
+                .HasColumnName("salary")
+                .HasColumnType("decimal(6,2)")
+                .IsRequired();
+
+            builder.Property(e => e.RowVersion)
+                .IsRowVersion();
+        }
+    }
+}
