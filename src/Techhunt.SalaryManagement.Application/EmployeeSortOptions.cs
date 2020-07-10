d5fa575feda8feebec405a83a9fe8a5b2bdd03c5
theNcoder <nisalwickramage@gmail.com>
2020-07-11 00:17:04 +0800

    Added employee repository definition.

---
+++ src/Techhunt.SalaryManagement.Application/EmployeeSortOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Techhunt.SalaryManagement.Application
+{
+    public struct EmployeeSortOptions
+    {
+        public Field Field { get; set; }
+
+        public Order Order { get; set; }
+    }
+
+    public enum Field
+    { 
+        Id,
+        Login,
+        Name,
+        Salary
+    }
+    public enum Order
+    {
+        Asc,
+        Desc
+    }
+}
---
+++ src/Techhunt.SalaryManagement.Application/IEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Techhunt.SalaryManagement.Domain;
+
+namespace Techhunt.SalaryManagement.Application
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> Get(
+            double minSalary, 
+            double maxSalary, 
+            long offset, 
+            int limit, 
+            EmployeeSortOptions sort);
+
+        Employee Get(string id);
+
+        void Delete(string id);
+
+        void Create(Employee employee);
+
+        void Create(IEnumerable<Employee> employees);
+
+        void Update(Employee employee);
+    }
+}
