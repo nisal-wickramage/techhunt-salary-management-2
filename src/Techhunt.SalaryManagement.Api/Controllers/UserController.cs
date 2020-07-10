7a08e82b4e0358e17d105727cbb34413fd66f121
theNcoder <nisalwickramage@gmail.com>
2020-07-10 18:01:25 +0800

    Added crud endpoints and search end point for users.

--- src/Techhunt.SalaryManagement.Api/Controllers/UserController.cs
+++ src/Techhunt.SalaryManagement.Api/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace Techhunt.SalaryManagement.Api.Controllers
@@ -12,6 +13,45 @@ namespace Techhunt.SalaryManagement.Api.Controllers
         public async Task<IActionResult> Upload()
         {
             return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(
+            decimal minSalary, 
+            decimal maxSalary, 
+            long offset, 
+            long limit, 
+            string sort)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("{id}")]
+        public async Task<IActionResult> Post([FromRoute] string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPatch]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Get([FromRoute]string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
