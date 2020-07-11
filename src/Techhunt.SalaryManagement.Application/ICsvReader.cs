b6e858c834524c760e1e82c53ba33b46fb1f4a78
theNcoder <nisalwickramage@gmail.com>
2020-07-11 21:49:05 +0800

    Added csv reader.

---
+++ src/Techhunt.SalaryManagement.Application/ICsvReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Techhunt.SalaryManagement.Domain;
+
+namespace Techhunt.SalaryManagement.Application
+{
+    public interface ICsvReader
+    {
+        IEnumerable<Employee> GetEmployees(MemoryStream stream);
+    }
+}
---
+++ src/Techhunt.SalaryManagement.Infrastructure/Csv/CsvReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Techhunt.SalaryManagement.Application;
+using Techhunt.SalaryManagement.Domain;
+
+namespace Techhunt.SalaryManagement.Infrastructure.Csv
+{
+    public class CsvReader : ICsvReader
+    {
+        public IEnumerable<Employee> GetEmployees(MemoryStream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
