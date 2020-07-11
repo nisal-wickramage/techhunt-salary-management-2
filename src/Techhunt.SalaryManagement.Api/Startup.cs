a75a7a4d7642bae700c556c11b708ad19b9e451e
theNcoder <nisalwickramage@gmail.com>
2020-07-11 21:17:00 +0800

    Wired up DI.

--- src/Techhunt.SalaryManagement.Api/Startup.cs
+++ src/Techhunt.SalaryManagement.Api/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Techhunt.SalaryManagement.Application;
+using Techhunt.SalaryManagement.Infrastructure.Persistance;
 
 namespace Techhunt.SalaryManagement.Api
 {
@@ -26,6 +22,10 @@ namespace Techhunt.SalaryManagement.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddTransient<EmployeeService>();
+            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+            services.AddDbContext<SalaryManagementDbContext>(options =>
+                options.UseInMemoryDatabase(databaseName: "SalaryManagement"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
