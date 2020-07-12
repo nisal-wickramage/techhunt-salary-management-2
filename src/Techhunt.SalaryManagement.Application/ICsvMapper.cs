b04537d32b81390f2d45f4884bcafcc79d751787
theNcoder <nisalwickramage@gmail.com>
2020-07-12 11:46:25 +0800

    Removed unused usings.

--- src/Techhunt.SalaryManagement.Application/ICsvMapper.cs
+++ src/Techhunt.SalaryManagement.Application/ICsvMapper.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Text;
-using System.Threading.Tasks;
 using Techhunt.SalaryManagement.Domain;
 
 namespace Techhunt.SalaryManagement.Application
--- src/Techhunt.SalaryManagement.Infrastructure/Csv/CsvMapper.cs
+++ src/Techhunt.SalaryManagement.Infrastructure/Csv/CsvMapper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Techhunt.SalaryManagement.Application;
 using Techhunt.SalaryManagement.Domain;
 
