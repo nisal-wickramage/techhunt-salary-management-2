362d9d0fb1b2611e4c45ba790266b4ea4ce62e6b
theNcoder <nisalwickramage@gmail.com>
2020-07-14 20:51:35 +0800

    Added menu panel component.

--- src/Techhunt.SalaryManagement.Ui/src/app/app.module.ts
+++ src/Techhunt.SalaryManagement.Ui/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MenuPanelComponent } from './components/menu-panel/menu-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MenuPanelComponent
   ],
   imports: [
     BrowserModule,
