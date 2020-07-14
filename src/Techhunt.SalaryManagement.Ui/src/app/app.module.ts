f6243d4f832025f0f746001c359351c8d35fa973
theNcoder <nisalwickramage@gmail.com>
2020-07-14 20:52:42 +0800

    Added search bar component.

--- src/Techhunt.SalaryManagement.Ui/src/app/app.module.ts
+++ src/Techhunt.SalaryManagement.Ui/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MenuPanelComponent } from './components/menu-panel/menu-panel.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MenuPanelComponent
+    MenuPanelComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
