904650a8d47cbf210a9a902ccebdcae3cddfd8b8
theNcoder <nisalwickramage@gmail.com>
2020-07-15 15:11:27 +0800

    Added build and run bat file to run application with docker.

---
+++ build.bat
@@ -0,0 +1,3 @@
+docker build -f src/Techhunt.SalaryManagement.Api/Dockerfile -t nisal/techhunt-api .
+cd src/Techhunt.SalaryManagement.Ui
+docker build -t nisal/techhunt-web .
---
+++ run.bat
@@ -0,0 +1,2 @@
+docker run -d -p 5000:80 --name myapp nisal/techhunt-api
+docker run -d -p 4200:80 nisal/techhunt-web
